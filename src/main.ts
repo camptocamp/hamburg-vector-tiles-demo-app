a80727b5561a98b1e465e1c22d7431c8bed031b4
Olivier Guyot <olivier.guyot@camptocamp.com>
2023-03-11 17:12:57 +0100

    add rendering analyzer

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,17 @@ import VectorTileSource from 'ol/build/ol/source/VectorTile'
 import WebGLTileLayer from 'ol/build/ol/layer/WebGLTile'
 import VectorTileLayer from 'ol/build/ol/layer/VectorTile'
 import WebGLVectorTileLayerRenderer from 'ol/build/ol/renderer/webgl/VectorTileLayer'
+import CanvasVectorTileLayerRenderer from 'ol/build/ol/renderer/canvas/VectorTileLayer'
 import MVT from 'ol/build/ol/format/MVT'
 import {asArray} from 'ol/build/ol/color';
 import {packColor} from 'ol/build/ol/renderer/webgl/shaders';
+import MixedGeometryBatch from 'ol/build/ol/render/webgl/MixedGeometryBatch';
+import TileGeometry from 'ol/build/ol/webgl/TileGeometry';
+import BuilderGroup from 'ol/build/ol/render/canvas/BuilderGroup';
+import ExecutorGroup from 'ol/build/ol/render/canvas/ExecutorGroup';
+import CompositeMapRenderer from 'ol/build/ol/renderer/Composite';
 import {applyStyle} from 'ol-mapbox-style/build/index'
+import {defineFrameContainer, trackPerformance, showTable, showGraph} from '@camptocamp/rendering-analyzer'
 
 class WebGLVectorTileLayer extends VectorTileLayer {
   createRenderer() {
@@ -50,13 +57,44 @@ class WebGLVectorTileLayer extends VectorTileLayer {
   }
 }
 
+const params = new URL(window.location).searchParams
+const useCanvas = params.get('canvas') !== null
+const showAnalyzer = params.get('analyzer') !== null
+
 async function initMap() {
-  const vectorTileLayer = new WebGLVectorTileLayer({
-    source: new VectorTileSource({
-      format: new MVT(),
-      url: 'https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/tiles/v1/bm_web_de_3857/{z}/{x}/{y}.pbf'
-    }),
+  const source = new VectorTileSource({
+    format: new MVT(),
+    url: 'https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/tiles/v1/bm_web_de_3857/{z}/{x}/{y}.pbf'
   })
+  let vectorTileLayer
+  if (useCanvas) {
+    vectorTileLayer = new VectorTileLayer({
+      source,
+      declutter: true
+    })
+  } else {
+    vectorTileLayer = new WebGLVectorTileLayer({
+      source
+    })
+  }
+
+  if (showAnalyzer) {
+    defineFrameContainer(CompositeMapRenderer, 'renderFrame');
+    trackPerformance(VectorTileSource);
+    if (useCanvas) {
+      trackPerformance(BuilderGroup);
+      trackPerformance(ExecutorGroup);
+      trackPerformance(CanvasVectorTileLayerRenderer);
+      trackPerformance(VectorTileLayer);
+    }
+    else {
+      trackPerformance(TileGeometry);
+      trackPerformance(MixedGeometryBatch);
+      trackPerformance(WebGLVectorTileLayerRenderer);
+    }
+    showGraph();
+    showTable();
+  }
 
   await applyStyle(vectorTileLayer, 'https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/styles/bm_web_col.json')
 
